6810263becfc329e4e858f5f4571c2df4e77c129
JaredMZ <jaredcv04@gmail.com>
2025-01-08 14:53:12 -0600

    function refactor

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,20 +1,18 @@
-import { useContext } from 'react';
-import { LanguageContext } from '../../contexts/LanguageContext';
-import './Button.css'
+import { useContext } from "react";
+import { LanguageContext } from "../../contexts/LanguageContext";
+import "./Button.css";
+import ButtonText from "./button-text.json";
+import { useTranslateObject } from "../../hooks/useTranslates/useTranslateObject";
 
 function Button() {
-    const { language, handleChangeLanguage } = useContext(LanguageContext);
+  const { language, handleChangeLanguage } = useContext(LanguageContext);
+  const { title } = useTranslateObject(ButtonText);
 
-    const toggleLanguage = () => {
-        const nextLanguage = language === "es" ? "en" : "es";
-        handleChangeLanguage(nextLanguage);
-    };
-
-    return (
-        <button onClick={toggleLanguage} className="language-button">
-            Cambiar Idioma
-        </button>
-    );
+  return (
+    <button onClick={() => handleChangeLanguage(language === "es" ? "en" : "es")} className="language-button">
+        {title}
+    </button>
+  );
 }
 
 export default Button;
