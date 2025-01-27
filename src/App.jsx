1a512fc849c08c910bc0e8d69f60138185379272
JaredMZ <jaredcv04@gmail.com>
2025-01-27 16:42:57 -0600

    added Cardlist component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Article from "./components/Article/Article";
 import Button from "./components/Button/Button";
+import CardList from "./components/CardList/CardList";
 import { LanguageProvider } from "./contexts/LanguageContext";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <LanguageProvider>
         <Article />
         <Button />
+        <CardList />
       </LanguageProvider>
     </>
   );
